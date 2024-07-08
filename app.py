4ed54e187790b82781b2a0e09b3be1362c04e523
PuteriIrdina <147248514+PuteriIrdina@users.noreply.github.com>
2024-07-08 10:35:48 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#The tittle, emoji from webfx emoji cheat sheet
+st.set_page_config(page_title="My Preloved item Recommendation", page_icon="😁", layout= "wide")
+
+#-----HEADER SECTION --------
+st.subheader("Hi, Welcome to my recommender website")
+st.title("What is this website for?", page_icon="🧐")
+st.write("This website will help you find the product you need and the seller to contact with")
