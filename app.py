2d584706c12f171bc6399daf0f47c66c536c2ee8
PuteriIrdina <147248514+PuteriIrdina@users.noreply.github.com>
2024-07-08 10:47:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ import streamlit as st
 st.set_page_config(page_title="My Preloved item Recommendation", page_icon="😁", layout= "wide")
 
 #-----HEADER SECTION --------
-st.subheader("Hi, Welcome to my recommender website")
-st.title("What is this website for?")
-st.write("This website will help you find the product you need and the seller to contact with")
+with st.container()
+  st.subheader("Hi, Welcome to my recommender website")
+  st.title("What is this website for?")
+  st.write("This website will help you find the product you need and the seller to contact with")
+
+#------ MAIN PAGE ------------
+with st.container():
+  st.write("----")
+  left_column, right_column = st.columns(2)
+  with left_column:
+    st.header("Register")
+    st.write("##")
+    st.write(""" 
+    So here you will register as a customer, you will insert you name, age, gender for the demographic and product category,condition and price for your 
+    product preference.
+    """) 
+
+    st.write("[Youtube Channel >](https://www.youtube.com/watch?v=Pe4gUSIxte8)")
